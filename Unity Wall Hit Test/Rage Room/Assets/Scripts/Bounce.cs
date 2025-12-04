611e1f5ac11e12c2c8255c6a557746d229c3e7cb
lorq8189 <112417035+lorq8189@users.noreply.github.com>
2025-12-04 01:06:21 -0500

    the baby oil will detonate

---
+++ Unity Wall Hit Test/Rage Room/Assets/Scripts/Bounce.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public ParticleSystem explosion1;
+    public ParticleSystem explosion2;
+    public Rigidbody rb;
+    
+    public ParticleSystem spark;
+    public float expThreshold = 10f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Vector3 vel = rb.linearVelocity;
+        if (vel.magnitude > expThreshold)
+        {
+            explosion1.transform.position = col.contacts[0].point;
+            explosion1.Play();
+            explosion2.transform.position = col.contacts[0].point;
+            explosion2.Play();
+            Destroy(gameObject);
+        }
+        else
+        {
+            spark.transform.position = col.contacts[0].point;
+            spark.Play();
+        }
+        
+
+        
+    }
+}
