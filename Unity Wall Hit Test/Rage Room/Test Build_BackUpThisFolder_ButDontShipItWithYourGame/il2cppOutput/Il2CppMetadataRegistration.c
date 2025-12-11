51777bc2634839b266d377bb69a9f54404ce2ce0
Kyle Kim <kkim198@jh.edu>
2025-12-10 19:26:17 -0500

    Title screen

---
+++ Unity Wall Hit Test/Rage Room/Test Build_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	21125,
+	g_Il2CppGenericTypes,
+	15260,
+	g_Il2CppGenericInstTable,
+	196023,
+	g_Il2CppGenericMethodFunctions,
+	64480,
+	g_Il2CppTypeTable,
+	228131,
+	g_Il2CppMethodSpecTable,
+	19414,
+	g_FieldOffsetTable,
+	19414,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
