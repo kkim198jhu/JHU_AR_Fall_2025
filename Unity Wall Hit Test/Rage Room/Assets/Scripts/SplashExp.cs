48bda1cd8d3e31c543f607853697704a9c499c75
lorq8189 <112417035+lorq8189@users.noreply.github.com>
2025-12-14 02:44:59 -0500

    i think i added the splash effects

---
+++ Unity Wall Hit Test/Rage Room/Assets/Scripts/SplashExp.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class Splash : MonoBehaviour
+{
+    [Header("VFX Graph prefabs (VisualEffect on the prefab root)")]
+    [SerializeField] private VisualEffect explosion1Prefab;
+    [SerializeField] private VisualEffect explosion2Prefab;
+    [SerializeField] private ParticleSystem sparkPrefab;
+
+
+    public float expThreshold = 2f;
+    private float vfxLifetime = 2f; 
+
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+
+        Vector3 vel = rb != null ? rb.linearVelocity : Vector3.zero;
+
+        ContactPoint cp = col.GetContact(0);
+        Vector3 hitPoint = cp.point;
+        Quaternion hitRot = Quaternion.LookRotation(cp.normal);
+
+        if (vel.magnitude > expThreshold)
+        {
+            SpawnAndPlayVFX(explosion1Prefab, hitPoint, hitRot);
+            SpawnAndPlayVFX(explosion2Prefab, hitPoint, hitRot);
+
+            Destroy(gameObject, 0.5f);
+        }
+        else
+        {
+            ParticleSystem ps = Instantiate(sparkPrefab, hitPoint, hitRot);
+            ps.Play();
+            Destroy(ps.gameObject, .1f);
+        }
+    }
+
+    private void SpawnAndPlayVFX(VisualEffect prefab, Vector3 pos, Quaternion rot)
+    {
+        if (prefab == null) return;
+
+        VisualEffect vfx = Instantiate(prefab, pos, rot);
+
+        //vfx.SendEvent("OnPlay");
+
+        vfx.Play();
+
+        Destroy(vfx.gameObject, vfxLifetime);
+    }
+}
